283e791b165aebea1b718114e6f5908477afdc39
Neil Lawrence <lawrennd@gmail.com>
2022-11-22 09:51:22 +0000

    Revert changes.

--- assets/js/download.js
+++ assets/js/download.js
@@ -14,17 +14,3 @@ function DownloadToFile(filename, containerid) {
     }
 }
 
-function DownloadTexToFile(filename, containerid) {
-    var pom = document.createElement('a');
-    pom.setAttribute('href', 'data:text/plain;charset=utf-8,' + document.getElementById(containerid).innerHTML);
-    pom.setAttribute('download', filename);
-
-    if (document.createEvent) {
-        var event = document.createEvent('MouseEvents');
-        event.initEvent('click', true, true);
-        pom.dispatchEvent(event);
-    }
-    else {
-        pom.click();
-    }
-}
